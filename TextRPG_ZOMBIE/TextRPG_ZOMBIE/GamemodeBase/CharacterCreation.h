2d1def0986b6a99ecf062200de1e04e848c5269a
leenaaq <v584731@gmail.com>
2025-01-16 21:16:31 +0900

    add : 캐릭터 닉네임 특수문자 방지

--- TextRPG_ZOMBIE/TextRPG_ZOMBIE/GamemodeBase/CharacterCreation.h
+++ TextRPG_ZOMBIE/TextRPG_ZOMBIE/GamemodeBase/CharacterCreation.h
@@ -4,6 +4,10 @@
 #include "../Character.h"
 #include <iostream>
 #include <windows.h>
+#include <cctype>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 class CharacterCreation {
