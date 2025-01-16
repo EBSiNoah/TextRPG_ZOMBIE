b0e11d8abf835027988abbaed115e3eff61bc4bc
김서영 <ksy99921@gmail.com>
2025-01-16 10:02:51 +0900

    🩹fix : 아이템 효과 문구, 조건, 화면 수정

--- TextRPG_ZOMBIE/TextRPG_ZOMBIE/Shop.cpp
+++ TextRPG_ZOMBIE/TextRPG_ZOMBIE/Shop.cpp
@@ -3,6 +3,8 @@
 #include "Item.h"
 #include <iomanip>
 #include <iostream>
+#include <string>
+#include <Windows.h>
 using namespace std;
 
 
@@ -14,6 +16,17 @@ Shop::Shop()
     // 아이템 리스트의 뒤에 계속 아이템 저장
     // 반복문의 int를 ItemType로 변경하여 ItemSetting 객체 생성 후 벡터에 저장
 }
+
+const string effect[] = {
+    "체력 +50",
+    "공격력 +10",
+    "공격력 +50",
+    "최대 체력 -30, 체력 +5000(약물 오용 주의)",
+    "최대 체력 +80",
+    "공격력 2배(3번의 전투로 제한)",
+    "피해 방허(1회용)",
+    "감염 상태 회복"
+};
 
 void Shop::displayItems(Character& character)
 {
@@ -48,9 +61,9 @@ void Shop::displayItems(Character& character)
     case 2:
         sellItem(character);
         break;
-    default:
+    default :
         break;
-
+// ****** 여기 수정 *****************
     }
 }
 
@@ -61,24 +74,29 @@ void Shop::buyItem(Character& character)
     cout << "구입을 원하는 자원 번호를 입력해주세요: ";
     cin >> item_idx;
 
-    if (item_idx < 0)
+    if (item_idx < 0 || item_idx >= itemList.size())
     {
-        cout << "해당 자원이 존재하지 않습니다." << endl;
+        cout << "아이템 목록에 있는 아이템만 구매할 수 있습니다." << endl;
         return;
     }
 
-    cout << itemList[item_idx].getName() << "자원 구매 개수를 입력해주세요: ";
+    cout << itemList[item_idx].getName() << "현재 보유 금액 : " << character.getMoney() << "  ||  자원 구매 개수를 입력해주세요: ";
     cin >> item_amount;
 
     if (character.getMoney() > item_amount * itemList[item_idx].getPrice())
     {
         character.addItem(item_idx, item_amount);
-        character.payMoney(itemList[item_idx].getPrice());
+        character.payMoney(itemList[item_idx].getPrice()*item_amount);
+        cout << "보유 중인 " << itemList[item_idx].getName() << " : " << character.getInventory()[item_idx].second
+            << " || 남은 돈 : " << character.getMoney() << endl;
+        Sleep(1000);
     }
     else
     {
         cout << "가진 돈 보다 많은 돈을 지불할 수 없습니다." << endl;
+        Sleep(1000);
     }
+    system("cls");
 
 }
 void Shop::sellItem(Character& character)
@@ -91,33 +109,38 @@ void Shop::sellItem(Character& character)
     if (item_idx < 0)
     {
         cout << "해당 자원이 존재하지 않습니다." << endl;
+        Sleep(1000);
         return;
     }
 
     if (character.getInventory()[item_idx].second > 0)
     {
-        cout << itemList[item_idx].getName() << "자원 판매 개수를 입력해주세요: ";
+        cout << itemList[item_idx].getName() << "현재 보유 개수: " << character.getInventory()[item_idx].second << "  ||  자원 판매 개수를 입력해주세요: ";
         cin >> item_amount;
 
         if (character.getInventory()[item_idx].second < item_amount)
         {
             cout << "보유중인 자원보다 더 많은 자원을 판매할 수 없습니다." << endl;
+            Sleep(1000);
             return;
         }
 
-        character.gainMoney(itemList[item_idx].getPrice() * 0.6);
+        character.gainMoney(itemList[item_idx].getPrice() * 0.6 * item_amount);
         character.deleteItem(item_idx, item_amount);
         cout << "남은 돈 : " << character.getMoney();
+        Sleep(1000);
     }
     else
     {
         cout << "보유 하지 않은 아이템을 판매할 수 없습니다." << endl;
+        Sleep(1000);
     }
-
+    system("cls");
 }
 
 Shop::~Shop()
 {
-    cout << setw(10) << "물자 거래소가 종료되었습니다. " << endl;
-    cout << "========================================================" << endl;
+    //cout << setw(10) << "물자 거래소가 종료되었습니다. " << endl;
+    //cout << "========================================================" << endl;
+
 }
